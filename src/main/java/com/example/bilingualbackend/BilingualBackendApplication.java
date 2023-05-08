46d687443917875620d3b7971655e0f1dd820658
Nurisa <nurisa.abdykalykova03@gmail.com>
2023-05-08 11:14:43 +0600

    first commit

---
+++ src/main/java/com/example/bilingualbackend/BilingualBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bilingualbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilingualBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilingualBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bilingualbackend/BilingualBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bilingualbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilingualBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
