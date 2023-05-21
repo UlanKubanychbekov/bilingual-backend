83b9b5f81b131d98fb7c797a7a87265f0974458f
SyimykRavshanbekov <programmist00707@gmail.com>
2023-05-21 20:49:52 +0600

    add graphql to signIn and signUp and create schemas

--- src/main/java/com/example/bilingualbackend/db/services/AuthenticationService.java
+++ src/main/java/com/example/bilingualbackend/db/services/AuthenticationService.java
@@ -1,7 +1,9 @@
 package com.example.bilingualbackend.db.services;
 
 import com.example.bilingualbackend.config.jwt.JwtService;
+import com.example.bilingualbackend.db.entities.User;
 import com.example.bilingualbackend.db.enums.Role;
+import com.example.bilingualbackend.db.repositories.UserRepository;
 import com.example.bilingualbackend.dto.requests.auth.AuthenticationRequest;
 import com.example.bilingualbackend.dto.requests.auth.SignUpRequest;
 import com.example.bilingualbackend.dto.responses.auth.AuthenticationResponse;
@@ -9,11 +11,7 @@ import com.example.bilingualbackend.exceptions.AlreadyExistException;
 import com.example.bilingualbackend.exceptions.BadCredentialException;
 import com.example.bilingualbackend.exceptions.BadRequestException;
 import com.example.bilingualbackend.exceptions.NotFoundException;
-import com.example.bilingualbackend.db.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
-import com.example.bilingualbackend.db.entities.User;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -23,7 +21,6 @@ public class AuthenticationService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
-    private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse signUp(SignUpRequest signUpRequest) {
         if (userRepository.existsByEmail(signUpRequest.getEmail())) {
