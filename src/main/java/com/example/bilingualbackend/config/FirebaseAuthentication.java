99c8561f3c0a73361a4a438915c1f2c94c287042
user <ulanskii03@gmail.com>
2023-05-23 11:58:13 +0600

    with google

---
+++ src/main/java/com/example/bilingualbackend/config/FirebaseAuthentication.java
@@ -0,0 +1,75 @@
+package com.example.bilingualbackend.config;
+
+import com.example.bilingualbackend.config.jwt.JwtService;
+import com.example.bilingualbackend.db.entities.User;
+import com.example.bilingualbackend.db.enums.Role;
+import com.example.bilingualbackend.db.repositories.UserRepository;
+import com.example.bilingualbackend.dto.responses.auth.AuthenticationResponse;
+import com.example.bilingualbackend.exceptions.NotFoundException;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+
+@Service
+public class FirebaseAuthentication {
+
+    private static final Logger log = LoggerFactory.getLogger(FirebaseAuthentication.class);
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @PostConstruct
+    void init() {
+        try {
+
+            GoogleCredentials googleCredentials = GoogleCredentials.fromStream(new ClassPathResource("bilingual-backend.json").getInputStream());
+            FirebaseOptions firebaseOptions = FirebaseOptions.builder()
+                    .setCredentials(googleCredentials)
+                    .build();
+            log.info("Firebase initialized successfully");
+            FirebaseApp firebaseApp = FirebaseApp.initializeApp(firebaseOptions);
+        } catch (IOException e) {
+            log.error("IOException occurred while initializing Firebase");
+        }
+    }
+
+
+    public AuthenticationResponse authWithGoogle(String tokenId) throws FirebaseAuthException {
+        FirebaseToken firebaseToken = FirebaseAuth.getInstance().verifyIdToken(tokenId);
+        if (!userRepository.existsByEmail(firebaseToken.getEmail())) {
+            User newUser = new User();
+            String[] name = firebaseToken.getName().split(" ");
+            newUser.setFirstName(name[0]);
+            newUser.setLastName(name[1]);
+            newUser.setEmail(firebaseToken.getEmail());
+            newUser.setPassword(firebaseToken.getEmail());
+            newUser.setRole(Role.USER);
+            userRepository.save(newUser);
+        }
+        User user = userRepository.findByEmail(firebaseToken.getEmail()).orElseThrow(() -> {
+            log.error("User not found for email {}", firebaseToken.getEmail());
+            throw new NotFoundException(String.format("User with this %s email not found !!", firebaseToken.getEmail()));
+        });
+        log.info("User authenticated successfully with Google for email {}", firebaseToken.getEmail());
+        String token = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .email(firebaseToken.getEmail())
+                .token(token)
+                .build();
+    }
+}
