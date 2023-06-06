0deb45ce0e72293d58a0514a7ce34ea87b0283c4
SyimykRavshanbekov <programmist00707@gmail.com>
2023-06-06 13:15:37 +0600

    change builder methods in QuestionService

--- src/main/java/com/example/bilingualbackend/db/services/QuestionService.java
+++ src/main/java/com/example/bilingualbackend/db/services/QuestionService.java
@@ -3,6 +3,7 @@ package com.example.bilingualbackend.db.services;
 import com.example.bilingualbackend.db.entities.Option;
 import com.example.bilingualbackend.db.entities.Question;
 import com.example.bilingualbackend.db.entities.Test;
+import com.example.bilingualbackend.db.enums.ContentType;
 import com.example.bilingualbackend.db.enums.QuestionType;
 import com.example.bilingualbackend.db.repositories.QuestionRepository;
 import com.example.bilingualbackend.db.repositories.TestRepository;
@@ -13,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -45,9 +47,8 @@ public class QuestionService {
         List<Option> options = request.getOptionRequests().stream()
                 .map(x -> Option.builder()
                         .isTrue(x.isCorrect())
-                        .value(x.getValue())
                         .question(question)
-                        .title(question.getQuestionType().name())
+                        .title(x.getValue())
                         .build())
                 .collect(Collectors.toList());
 
@@ -74,7 +75,7 @@ public class QuestionService {
         Question question = Question.builder()
                 .questionType(QuestionType.RECORD_SAYING_STATEMENT)
                 .title(request.getTitle())
-                .correctAnswer(request.getStatement())
+                .value(Map.of(ContentType.TEXT, request.getStatement()))
                 .duration(request.getDuration())
                 .test(test)
                 .enable(request.isActive())
