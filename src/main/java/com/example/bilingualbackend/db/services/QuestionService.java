568f075a77b037d7468574bf0454ed0adb2c7848
as-aijamal <a.asangazieva@gmail.com>
2023-06-08 15:05:49 +0600

    question type what you hear

--- src/main/java/com/example/bilingualbackend/db/services/QuestionService.java
+++ src/main/java/com/example/bilingualbackend/db/services/QuestionService.java
@@ -141,6 +141,27 @@ public class QuestionService {
                 .build();
     }
 
+    public SimpleResponse saveTypeWhatYouHear(QuestionMainRequest questionRequest) {
+        Test test = testRepository.findById(questionRequest.getTestId()).orElseThrow(
+                () -> new NotFoundException("Test with id: " + questionRequest.getTestId() + " doesn't exist"));
+
+        Question question = Question.builder()
+                .title(questionRequest.getTitle())
+                .questionType(QuestionType.TYPE_WHAT_YOU_HEAR)
+                .count(questionRequest.getCount())
+                .value(Map.of(ContentType.AUDIO, questionRequest.getValue()))
+                .correctAnswer(questionRequest.getCorrectAnswer())
+                .duration(questionRequest.getDuration())
+                .test(test)
+                .build();
+        questionRepository.save(question);
+
+        return SimpleResponse.builder()
+                .message("Question : "+questionRequest.getTitle()+" successfully saved")
+                .build();
+    }
+
+
     public SimpleResponse saveQuestion(QuestionMainRequest questionMainRequest) {
         switch (questionMainRequest.getQuestionType()) {
             case SELECT_ENGLISH_WORD -> {
@@ -154,6 +175,9 @@ public class QuestionService {
             }
             case SELECT_THE_MAIN_IDEA -> {
                 return saveSelectMainIdeaQuestion(questionMainRequest);
+            }
+            case TYPE_WHAT_YOU_HEAR->{
+                return saveTypeWhatYouHear(questionMainRequest);
             }
             default -> {
                 return SimpleResponse.builder()
--- src/main/java/com/example/bilingualbackend/dto/responses/QuestionResponse.java
+++ src/main/java/com/example/bilingualbackend/dto/responses/QuestionResponse.java
@@ -1,6 +1,9 @@
-package com.example.bilingualbackend.dto.requests.question;
+package com.example.bilingualbackend.dto.responses;
 
 import com.example.bilingualbackend.db.enums.QuestionType;
+import com.example.bilingualbackend.dto.requests.question.OptionSelectMainIdeaRequest;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,7 +11,9 @@ import java.util.List;
 
 @Getter
 @Setter
-public class QuestionMainRequest {
+@AllArgsConstructor
+@Builder
+public class QuestionResponse {
     private QuestionType questionType;
     private String title;
     private Integer duration;
